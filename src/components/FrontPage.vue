79a4dd551a36b715f59ac27baff89b58267a526e
hamzakhomri <hamza.khomri36@gmail.com>
2023-08-20 15:39:56 +0100

    import tailwindcss

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -37,7 +37,7 @@
     - MODAL
   -->
 
-  <div class="modal" :class="{ 'closed': isModalClosed }"  data-modal>
+  <!-- <div class="modal" :class="{ 'closed': isModalClosed }"  data-modal>
 
     <div class="modal-close-overlay" data-modal-overlay></div>
 
@@ -76,7 +76,7 @@
 
     </div>
 
-  </div>
+  </div> -->
 
 
 
@@ -492,269 +492,144 @@
         </ul>
       </div>
     </nav>
+    <template v-for="(menu, index) in mobileMenus" :key="index">
 
-    <div class="mobile-bottom-navigation">
+      <div  class="mobile-bottom-navigation">
 
-      <button class="action-btn" data-mobile-menu-open-btn>
-        <ion-icon name="menu-outline"></ion-icon>
-      </button>
-
-      <button class="action-btn" @click="openMobileMenu(0)">
-      <ion-icon name="bag-handle-outline"></ion-icon>
-      <span class="count">0</span>
-    </button>
-
-      <button class="action-btn">
-        <ion-icon name="home-outline"></ion-icon>
-      </button>
-
-      <button class="action-btn">
-        <ion-icon name="heart-outline"></ion-icon>
-
-        <span class="count">0</span>
-      </button>
-
-      <button class="action-btn" data-mobile-menu-open-btn>
-        <ion-icon name="grid-outline"></ion-icon>
-      </button>
-
-    </div>
-
-    <nav class="mobile-navigation-menu has-scrollbar" v-if="mobileMenus[0].isActive" data-mobile-menu>
-
-      <div class="menu-top">
-        <h2 class="menu-title">Menu</h2>
-
-        <button class="menu-close-btn" data-mobile-menu-close-btn>
-          <ion-icon name="close-outline"></ion-icon>
+        <button @click="openMobileMenu(index)" data-mobile-menu-open-btn>
+          <ion-icon name="menu-outline"></ion-icon>
         </button>
+
+        <button class="action-btn" >
+          <ion-icon name="bag-handle-outline"></ion-icon>
+          <span class="count">0</span>
+        </button>
+
+        <button class="action-btn">
+          <ion-icon name="home-outline"></ion-icon>
+        </button>
+
+        <button class="action-btn">
+          <ion-icon name="heart-outline"></ion-icon>
+
+          <span class="count">0</span>
+        </button>
+
+        <button class="action-btn" data-mobile-menu-open-btn>
+          <ion-icon name="grid-outline"></ion-icon>
+        </button>
+
       </div>
 
-      <ul class="mobile-menu-category-list"  style="background-color: coral;" >
+      <nav class="mobile-navigation-menu has-scrollbar" :class="{ 'active': menu.isActive }" data-mobile-menu>
 
-        <li class="menu-category">
-          <a href="#" class="menu-title">Home</a>
-        </li>
-        <!--  Début  -->
-        <li class="menu-category">
+        <div class="menu-top">
+          <h2 class="menu-title">Menu</h2>
 
-            <button class="accordion-menu" data-accordion-btn>
-              <p class="menu-title">Men's</p>
-              <div>
-                <ion-icon name="add-outline" class="add-icon"></ion-icon>
-                <ion-icon name="remove-outline" class="remove-icon"></ion-icon>
+          <button class="menu-close-btn"  @click="closeMobileMenu(index)" data-mobile-menu-close-btn>
+            <ion-icon name="close-outline"></ion-icon>
+          </button>
+        </div>
+
+        <template v-for="(item, index) in accordionItems" :key="index">
+          <li class="menu-category">
+            <button @click="toggleAccordion(index)" data-accordion-btn class="accordion-button">
+              <div class="accordion-menu-navig" data-accordion-btn> 
+                <p class="menu-title">{{ item.title }}</p>
+                <ion-icon v-if="!item.isActive" name="add-outline" class="icon"></ion-icon>
+                <ion-icon v-if="item.isActive" name="remove-outline" class="icon"></ion-icon>
               </div>
             </button>
-
-            <ul class="submenu-category-list" data-accordion>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">Shirt</a>
-              </li>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">Shorts & Jeans</a>
-              </li>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">Safety Shoes</a>
-              </li>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">Wallet</a>
+            <ul class="submenu-category-list" :class="{ 'active': item.isActive }" data-accordion>
+              <li class="submenu-category" v-for="(submenu, subIndex) in item.submenus" :key="subIndex">
+                <a href="#" class="submenu-title">{{ submenu.title }}</a>
               </li>
             </ul>
-        </li>
+          </li>
+        </template>
 
-        <li class="menu-category">
 
-          <button class="accordion-menu" data-accordion-btn>
-            <p class="menu-title">Women's</p>
+        <div class="menu-bottom">
 
-            <div>
-              <ion-icon name="add-outline" class="add-icon"></ion-icon>
-              <ion-icon name="remove-outline" class="remove-icon"></ion-icon>
-            </div>
-          </button>
+          <ul class="menu-category-list">
 
-          <ul class="submenu-category-list" data-accordion>
+            <li class="menu-category">
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Dress & Frock</a>
+              <button class="accordion-menu" data-accordion-btn>
+                <p class="menu-title">Language</p>
+
+                <ion-icon name="caret-back-outline" class="caret-back"></ion-icon>
+              </button>
+
+              <ul class="submenu-category-list" data-accordion>
+
+                <li class="submenu-category">
+                  <a href="#" class="submenu-title">English</a>
+                </li>
+
+                <li class="submenu-category">
+                  <a href="#" class="submenu-title">Espa&ntilde;ol</a>
+                </li>
+
+                <li class="submenu-category">
+                  <a href="#" class="submenu-title">Fren&ccedil;h</a>
+                </li>
+
+              </ul>
+
             </li>
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Earrings</a>
-            </li>
+            <li class="menu-category">
+              <button class="accordion-menu" data-accordion-btn>
+                <p class="menu-title">Currency</p>
+                <ion-icon name="caret-back-outline" class="caret-back"></ion-icon>
+              </button>
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Necklace</a>
-            </li>
+              <ul class="submenu-category-list" data-accordion>
+                <li class="submenu-category">
+                  <a href="#" class="submenu-title">USD &dollar;</a>
+                </li>
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Makeup Kit</a>
+                <li class="submenu-category">
+                  <a href="#" class="submenu-title">EUR &euro;</a>
+                </li>
+              </ul>
             </li>
 
           </ul>
 
-        </li>
+          <ul class="menu-social-container">
 
-        <li class="menu-category">
-
-          <button class="accordion-menu" data-accordion-btn>
-            <p class="menu-title">Jewelry</p>
-
-            <div>
-              <ion-icon name="add-outline" class="add-icon"></ion-icon>
-              <ion-icon name="remove-outline" class="remove-icon"></ion-icon>
-            </div>
-          </button>
-
-          <ul class="submenu-category-list" data-accordion>
-
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Earrings</a>
+            <li>
+              <a href="#" class="social-link">
+                <ion-icon name="logo-facebook"></ion-icon>
+              </a>
             </li>
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Couple Rings</a>
+            <li>
+              <a href="#" class="social-link">
+                <ion-icon name="logo-twitter"></ion-icon>
+              </a>
             </li>
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Necklace</a>
+            <li>
+              <a href="#" class="social-link">
+                <ion-icon name="logo-instagram"></ion-icon>
+              </a>
             </li>
 
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Bracelets</a>
+            <li>
+              <a href="#" class="social-link">
+                <ion-icon name="logo-linkedin"></ion-icon>
+              </a>
             </li>
 
           </ul>
 
-        </li>
+        </div>
 
-        <li class="menu-category">
-          <button class="accordion-menu" data-accordion-btn>
-            <p class="menu-title">Perfume</p>
-
-            <div>
-              <ion-icon name="add-outline" class="add-icon"></ion-icon>
-              <ion-icon name="remove-outline" class="remove-icon"></ion-icon>
-            </div>
-          </button>
-
-          <ul class="submenu-category-list" data-accordion>
-
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Clothes Perfume</a>
-            </li>
-
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Deodorant</a>
-            </li>
-
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Flower Fragrance</a>
-            </li>
-
-            <li class="submenu-category">
-              <a href="#" class="submenu-title">Air Freshener</a>
-            </li>
-
-          </ul>
-
-        </li>
-
-        <li class="menu-category">
-          <a href="#" class="menu-title">Blog</a>
-        </li>
-
-        <li class="menu-category">
-          <a href="#" class="menu-title">Hot Offers</a>
-        </li>
-
-      </ul>
-
-      <div class="menu-bottom">
-
-        <ul class="menu-category-list">
-
-          <li class="menu-category">
-
-            <button class="accordion-menu" data-accordion-btn>
-              <p class="menu-title">Language</p>
-
-              <ion-icon name="caret-back-outline" class="caret-back"></ion-icon>
-            </button>
-
-            <ul class="submenu-category-list" data-accordion>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">English</a>
-              </li>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">Espa&ntilde;ol</a>
-              </li>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">Fren&ccedil;h</a>
-              </li>
-
-            </ul>
-
-          </li>
-
-          <li class="menu-category">
-            <button class="accordion-menu" data-accordion-btn>
-              <p class="menu-title">Currency</p>
-              <ion-icon name="caret-back-outline" class="caret-back"></ion-icon>
-            </button>
-
-            <ul class="submenu-category-list" data-accordion>
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">USD &dollar;</a>
-              </li>
-
-              <li class="submenu-category">
-                <a href="#" class="submenu-title">EUR &euro;</a>
-              </li>
-            </ul>
-          </li>
-
-        </ul>
-
-        <ul class="menu-social-container">
-
-          <li>
-            <a href="#" class="social-link">
-              <ion-icon name="logo-facebook"></ion-icon>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" class="social-link">
-              <ion-icon name="logo-twitter"></ion-icon>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" class="social-link">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-          </li>
-
-          <li>
-            <a href="#" class="social-link">
-              <ion-icon name="logo-linkedin"></ion-icon>
-            </a>
-          </li>
-
-        </ul>
-
-      </div>
-
-    </nav>
-
+      </nav>
+    </template>
   </header>
 
 
@@ -3599,7 +3474,35 @@ export default {
       isToastClosed: true,
       mobileMenus: Array.from({ length: 3 }, () => ({ isActive: false })),
       isOverlayActive: false,
-      accordionItems: Array.from({ length: 3 }, () => ({ isActive: false })),
+      // accordionItems: Array.from({ length: 3 }, () => ({ isActive: false })),
+
+
+
+      accordionItems: [
+        {
+          title: "Men's",
+          isActive: false,
+          submenus: [
+            { title: 'Shirt' },
+            { title: 'Shorts & Jeans' },
+            { title: 'Safety Shoes' },
+            { title: 'Wallet' }
+          ]
+        },
+        {
+          title: "Women's",
+          isActive: false,
+          submenus: [
+            { title: 'Dress & Frock' },
+            { title: 'Earrings' },
+            { title: 'Necklace' },
+            { title: 'Makeup Kit' }
+          ]
+        },
+        // ...other accordion items...
+      ],
+    
+
     };
   },
   methods: {
